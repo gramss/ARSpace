64ed3ba570764fb3f66e34330afe9e27857f0cce
Flo <flo.gramss@gmail.com>
2017-11-05 00:13:00 +0100

    Initial Commit

---
+++ ARSpaceTests/ARSpaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARSpaceTests.swift
+//  ARSpaceTests
+//
+//  Created by Florian Gramß on 05.11.17.
+//  Copyright © 2017 Florian Gramß. All rights reserved.
+//
+
+import XCTest
+@testable import ARSpace
+
+class ARSpaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
