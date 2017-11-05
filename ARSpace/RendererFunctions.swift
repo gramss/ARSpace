665e0dd34efccaf0d39e46b53f1429f6a28c26ca
matthi97 <matthi97@hotmail.de>
2017-11-05 03:11:39 +0100

    added RenderFunctions

---
+++ ARSpace/RendererFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  RendererFunctions.swift
+//  ARSpace
+//
+//  Created by Matthias Vietz on 05.11.17.
+//  Copyright © 2017 Florian Gramß. All rights reserved.
+//
+
+import SceneKit
+import ARKit
+func Renderer_Add(didAdd node: SCNNode, for anchor: ARAnchor) -> Bool {
+    
+    return true
+}
